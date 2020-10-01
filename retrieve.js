11894e70a5902e15c8e926d0a7701c234a8789ab
jayharris14 <70274419+jayharris14@users.noreply.github.com>
2020-10-01 12:10:21 -0400

    Update retrieve.js

--- retrieve.js
+++ retrieve.js
@@ -6,46 +6,68 @@ console.log("penguins", penguin);
 drawTable(penguin)}
 
 var failureFCN= function(error)
-{console.log(penguin)}
+{console.log("error", error)}
 
 penguinParadise.then(successFCN, failureFCN)
 
+var quizmean=function(penguin)
+{var getQuizgrades=function(quiz){
+  return quiz.grade
+}
+var quizGrades=penguin.quizes.map(getQuizgrades)
+var quizmean=d3.mean(quizGrades)
+return quizmean}
 
+
+var homeworkmean=function(penguin)
+{var gethwgrades=function(homework){
+  return homework.grade
+}
+var hwGrades=penguin.homework.map(gethwgrades)
+var homeworkmean=d3.mean(hwGrades)
+return homeworkmean}
+
+var testmean=function(penguin)
+{var gettestgrades=function(test){
+  return test.grade
+}
+var testGrades=penguin.test.map(gettestgrades)
+var testmean=d3.mean(testGrades)
+return testmean}
+
+var finalmean=function(penguin)
+{var getfinalgrades=function(final){
+  return final.grade
+}
+var finalGrades=penguin.final.map(getfinalgrades)
+var finalmean=d3.mean(finalGrades)
+return finalmean}
 
 var drawTable= function(penguin)
 {
   console.log ("hi", penguin)
-var rows=d3.select("employeeTable tbody")
+var rows=d3.select("#tab")
   .selectAll("tr")
   .data(penguin)
   .enter()
   .append("tr")
 
   rows.append("td")
-  .text(function(penguin)
-  {return penguin.picture})
-
-  rows.append("td")
   .append("img")
   .attr ("src", function(penguin)
-  {return penguin.picture})
+  {return "imgs/"+ penguin.picture})
 
   rows.append("td")
-  .text(function(penguin)
-{return penguin.quizmean})
+  .text(quizmean)
 
 rows.append("td")
-.text(function(penguin)
-{return penguin.homeworkmean})
+.text(homeworkmean)
 
 rows.append("td")
-.text(function(penguin)
-{return penguin.testmean})
+.text(testmean)
 
 rows.append("td")
-.text(function(penguin)
-{return penguin.finalgrade})
-
+.text(finalmean)
 
 }
 
@@ -92,7 +114,7 @@ homeworkmean.sort(function(charA,charB)
             return 0;
         }
         else if (charA.homeworkmean > 
-                        charB.homeworkmean))
+                        charB.homeworkmean)
         {
             return 1;
         }
@@ -109,7 +131,7 @@ finalmean.sort(function(charA,charB)
             return 0;
         }
         else if (charA.finalmean > 
-                        charB.finalmean))
+                        charB.finalmean)
         {
             return 1;
         }
