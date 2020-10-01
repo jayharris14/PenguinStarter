337ffdf76f7087a25135d73479f646c554c23076
jayharris14 <70274419+jayharris14@users.noreply.github.com>
2020-10-01 12:09:01 -0400

    Update retrieve.js

--- retrieve.js
+++ retrieve.js
@@ -48,3 +48,74 @@ rows.append("td")
 
 
 }
+
+quizmean.sort(function(charA,charB)
+    {
+        if(charA.quizmean ==
+                        charB.quizmean)
+        {
+            return 0;
+        }
+        else if (charA.quizmean > 
+                        charB.quizmean))
+        {
+            return 1;
+        }
+        else    //char is <=100
+        {
+            return -1;
+        }
+    });
+
+testmean.sort(function(charA,charB)
+    {
+        if(charA.testmean ==
+                        charB.testmean)
+        {
+            return 0;
+        }
+        else if (charA.testmean > 
+                        charB.testmean))
+        {
+            return 1;
+        }
+        else    //char is <=100
+        {
+            return -1;
+        }
+
+homeworkmean.sort(function(charA,charB)
+    {
+        if(charA.homeworkmean ==
+                        charB.homeworkmean)
+        {
+            return 0;
+        }
+        else if (charA.homeworkmean > 
+                        charB.homeworkmean))
+        {
+            return 1;
+        }
+        else    //char is <=100
+        {
+            return -1;
+        }
+
+finalmean.sort(function(charA,charB)
+    {
+        if(charA.finalmean ==
+                        charB.finalmean)
+        {
+            return 0;
+        }
+        else if (charA.finalmean > 
+                        charB.finalmean))
+        {
+            return 1;
+        }
+        else    //char is <=100
+        {
+            return -1;
+        }
+
+
